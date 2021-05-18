2cde6569ffff39b3f253956f5bbcb83badefc234
Marciano Schildmeijer <marciano@kabisa.nl>
2021-05-18 13:05:53 +0200

    Add toggler for game and selection screen.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import GameContainer from "./components/GameContainer";
+import LevelSelectScreenContainer from "./components/LevelSelectScreenContainer";
 import { GameStateProvider } from "./providers/GameStateProvider";
 import "./App.css";
 
 function App() {
+	const [showGame, setShowGame] = useState(false);
 	return (
 		<GameStateProvider>
-			<GameContainer />
+			<button onClick={() => setShowGame(!showGame)}>Toggle mode</button>
+			{showGame && <GameContainer />}
+			{!showGame && <LevelSelectScreenContainer />}
 		</GameStateProvider>
 	);
 }
