c7c41ae3b3b17d7aaadd36ac061812fc38f447f7
Marciano Schildmeijer <marciano@kabisa.nl>
2021-05-21 23:23:30 +0200

    Add level config.

--- src/data/levelMappings.ts
+++ src/data/levelMappings.ts
@@ -1,70 +1,173 @@
 import { BrickType } from "../types/Bricks";
 
-export const bricksMapping: BrickType[][] = [
-	[
-		{ id: 1, size: "default", willDrop: true, cracked: false },
-		{ id: 2, size: "default", willDrop: true, cracked: false },
-		{ id: 3, size: "default", willDrop: true, cracked: false },
-		{ id: 4, size: "default", willDrop: true, cracked: false },
-		{ id: 5, size: "default", willDrop: true, cracked: false },
+type Level = {
+	name: string;
+	level: number;
+	layout: BrickType[][];
+};
+
+const LEVEL_ONE: Level = {
+	name: "",
+	level: 1,
+	layout: [
+		[
+			{ id: 1, size: "default", willDrop: true, cracked: false },
+			{ id: 2, size: "default", willDrop: true, cracked: false },
+			{ id: 3, size: "default", willDrop: true, cracked: false },
+			{ id: 4, size: "default", willDrop: true, cracked: false },
+			{ id: 5, size: "default", willDrop: true, cracked: false },
+		],
+		[
+			{
+				id: 6,
+				size: "small",
+				willDrop: true,
+				cracked: false,
+			},
+			{ id: 7, size: "default", willDrop: true, cracked: false },
+			{
+				id: 8,
+				size: "default",
+				willDrop: true,
+				cracked: false,
+			},
+			{
+				id: 9,
+				size: "default",
+				willDrop: true,
+				cracked: false,
+			},
+			{
+				id: 10,
+				size: "default",
+				willDrop: true,
+				cracked: false,
+			},
+			{
+				id: 11,
+				size: "small",
+				willDrop: true,
+				cracked: false,
+			},
+		],
+		[
+			{ id: 12, size: "default", willDrop: true, cracked: false },
+			{
+				id: 13,
+				size: "default",
+				willDrop: true,
+				cracked: false,
+			},
+			{
+				id: 14,
+				size: "small",
+				willDrop: true,
+				cracked: false,
+			},
+			{ id: 15, size: "default", willDrop: true, cracked: false },
+			{
+				id: 16,
+				size: "small",
+				willDrop: true,
+				cracked: false,
+			},
+			{
+				id: 17,
+				size: "default",
+				willDrop: true,
+				cracked: false,
+			},
+		],
+		[
+			{ id: 18, size: "default", willDrop: true, cracked: false },
+			{ id: 19, size: "default", willDrop: true, cracked: false },
+			{ id: 20, size: "default", willDrop: true, cracked: false },
+			{ id: 21, size: "default", willDrop: true, cracked: false },
+			{ id: 22, size: "default", willDrop: true, cracked: false },
+		],
 	],
-	[
-		{
-			id: 6,
-			size: "small",
-			willDrop: true, cracked: false,
-		},
-		{ id: 7, size: "default", willDrop: true, cracked: false },
-		{
-			id: 8,
-			size: "default",
-			willDrop: true, cracked: false,
-		},
-		{
-			id: 9,
-			size: "default",
-			willDrop: true, cracked: false,
-		},
-		{
-			id: 10,
-			size: "default",
-			willDrop: true, cracked: false,
-		},
-		{
-			id: 11,
-			size: "small",
-			willDrop: true, cracked: false,
-		},
+};
+
+const LEVEL_TWO: Level = {
+	name: "",
+	level: 2,
+	layout: [
+		[
+			{ id: 1, size: "default", willDrop: true, cracked: false },
+			{ id: 2, size: "default", willDrop: true, cracked: false },
+			{ id: 3, size: "default", willDrop: true, cracked: false },
+			{ id: 4, size: "default", willDrop: true, cracked: false },
+			{ id: 5, size: "default", willDrop: true, cracked: false },
+		],
+		[
+			{
+				id: 6,
+				size: "small",
+				willDrop: true,
+				cracked: false,
+			},
+			{ id: 7, size: "default", willDrop: true, cracked: false },
+			{
+				id: 8,
+				size: "default",
+				willDrop: true,
+				cracked: false,
+			},
+			{
+				id: 9,
+				size: "default",
+				willDrop: true,
+				cracked: false,
+			},
+			{
+				id: 10,
+				size: "default",
+				willDrop: true,
+				cracked: false,
+			},
+			{
+				id: 11,
+				size: "small",
+				willDrop: true,
+				cracked: false,
+			},
+		],
+		[
+			{ id: 12, size: "default", willDrop: true, cracked: false },
+			{
+				id: 13,
+				size: "default",
+				willDrop: true,
+				cracked: false,
+			},
+			{
+				id: 14,
+				size: "small",
+				willDrop: true,
+				cracked: false,
+			},
+			{ id: 15, size: "default", willDrop: true, cracked: false },
+			{
+				id: 16,
+				size: "small",
+				willDrop: true,
+				cracked: false,
+			},
+			{
+				id: 17,
+				size: "default",
+				willDrop: true,
+				cracked: false,
+			},
+		],
+		[
+			{ id: 18, size: "default", willDrop: true, cracked: false },
+			{ id: 19, size: "default", willDrop: true, cracked: false },
+			{ id: 20, size: "default", willDrop: true, cracked: false },
+			{ id: 21, size: "default", willDrop: true, cracked: false },
+			{ id: 22, size: "default", willDrop: true, cracked: false },
+		],
 	],
-	[
-		{ id: 12, size: "default", willDrop: true, cracked: false },
-		{
-			id: 13,
-			size: "default",
-			willDrop: true, cracked: false,
-		},
-		{
-			id: 14,
-			size: "small",
-			willDrop: true, cracked: false,
-		},
-		{ id: 15, size: "default", willDrop: true, cracked: false },
-		{
-			id: 16,
-			size: "small",
-			willDrop: true, cracked: false,
-		},
-		{
-			id: 17,
-			size: "default",
-			willDrop: true, cracked: false,
-		},
-	],
-	[
-		{ id: 18, size: "default", willDrop: true, cracked: false },
-		{ id: 19, size: "default", willDrop: true, cracked: false },
-		{ id: 20, size: "default", willDrop: true, cracked: false },
-		{ id: 21, size: "default", willDrop: true, cracked: false },
-		{ id: 22, size: "default", willDrop: true, cracked: false },
-	],
-];
+};
+
+export const levels: Level[] = [LEVEL_ONE, LEVEL_TWO];
